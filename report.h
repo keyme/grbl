7ac5e4d74c36ec7f1c909edcffb4fb59a36bf426
Adit Namdev <aditn4@gmail.com>
2016-06-18 12:36:41 -0400

    Made refactoring changes based on Pull request comments

--- report.h
+++ report.h
@@ -64,7 +64,7 @@
 #define MESSAGE_DISABLED 5
 
 // Variable for holding voltage value after ADC
-extern uint32_t voltage_result[5];
+extern uint32_t voltage_result[VOLT_NUM];
 extern uint8_t voltage_result_index;
 
 // Prints system status messages.
