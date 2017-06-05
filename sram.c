8b8f5d8bea4e54c9ec984b5c8871268806c99253
Alan Davidson <alan@key.me>
2017-06-05 12:06:56 -0400

    PR feedback

--- sram.c
+++ sram.c
@@ -34,31 +34,6 @@ void sram_init()
   sram_set_mode(BYTE_MODE);
 }
 
-uint8_t _sram_mode_helper(uint8_t * data_out, uint8_t len)
-{
-  spi_set_mode(0, 0);
-
-  uint8_t data_in[len];
-
-  bit_false(SCS_SRAM_PORT, 1 << SCS_SRAM_PIN);
-  spi_transact_array(data_out, data_in, len);
-  bit_true(SCS_SRAM_PORT, 1 << SCS_SRAM_PIN);
-
-  return (data_in[len - 1] & MODE_MASK) >> MODE_IDX;
-}
-
-uint8_t sram_read_mode()
-{
-  uint8_t data_out[2] = {RDMR, 0xFF};
-  return _sram_mode_helper(data_out, 2);
-}
-
-void sram_set_mode(enum sram_mode_e mode)
-{
-  uint8_t data_out[2] = {WRMR, mode << MODE_IDX};
-  _sram_mode_helper(data_out, 2);
-}
-
 uint8_t _sram_transact_helper(uint8_t * data_out, uint8_t len)
 {
   /* Transacts the array data_out over SPI to the SRAM IC
@@ -73,6 +48,18 @@ uint8_t _sram_transact_helper(uint8_t * data_out, uint8_t len)
 
   return data_in[len - 1];
 
+}
+
+uint8_t sram_read_mode()
+{
+  uint8_t data_out[2] = {RDMR, 0xFF};
+  return (_sram_transact_helper(data_out, 2) & MODE_MASK) >> MODE_IDX;
+}
+
+void sram_set_mode(enum sram_mode_e mode)
+{
+  uint8_t data_out[2] = {WRMR, mode << MODE_IDX};
+  _sram_transact_helper(data_out, 2);
 }
 
 uint8_t sram_read_byte(uint16_t addr)
